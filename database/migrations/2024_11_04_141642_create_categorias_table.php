9ee45aefb710c5de1e44f89f8c30e7976bfa7c3e
Felipe Martins <felipecardoso1508@gmail.com>
2024-11-04 13:58:44 -0300

    Adicionando categoria

--- database/migrations/2024_11_04_141642_create_categorias_table.php
+++ database/migrations/2024_11_04_141642_create_categorias_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('produtos', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->string('name_product');
+            $table->string('name');
             $table->text('description');
-            $table->string('category');
-            $table->string('mark');
-            $table->decimal('price', 10, 2);
-
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('produtos');
+        Schema::dropIfExists('categorias');
     }
 };
